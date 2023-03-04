4345cc6aa08af0d75aa4269131788b05203aeff4
crystal <crystal@test.com>
2023-03-04 19:02:34 +0900

    initial

--- OnDevice/app/src/main/java/com/example/facedetection/MainActivity.kt
+++ OnDevice/app/src/main/java/com/example/facedetection/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.facedetection
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
 
         val img: ImageView = findViewById(R.id.imageFace)
@@ -23,6 +26,47 @@ class MainActivity : AppCompatActivity() {
         bitmap?.apply{
             img.setImageBitmap(this)
         }
+
+        val btn: Button = findViewById(R.id.btnTest)
+        btn.setOnClickListener {
+            val highAccuracyOpts = FaceDetectorOptions.Builder()
+                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                .build()
+            val detector = FaceDetection.getClient(highAccuracyOpts)
+            val image = InputImage.fromBitmap(bitmap!!, 0)
+            val result = detector.process(image)
+                .addOnSuccessListener { faces ->
+                    // Task completed successfully
+                    // ...
+                    bitmap?.apply {
+                        img.setImageBitmap(drawWithRectangle(faces))
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    // ...
+                }
+        }
+    }
+
+
+    fun Bitmap.drawWithRectangle(faces: List<Face>):Bitmap?{
+        val bitmap = copy(config, true)
+        val canvas = Canvas(bitmap)
+        for (face in faces){
+            val bounds = face.boundingBox
+            Paint().apply {
+                color = Color.RED
+                style = Paint.Style.STROKE
+                strokeWidth = 4.0f
+                isAntiAlias = true
+                // draw rectangle on canvas
+                canvas.drawRect(
+                    bounds, this
+                )
+            }
+        }
+        return bitmap
     }
 
     // helper function to get bitmap from assets
