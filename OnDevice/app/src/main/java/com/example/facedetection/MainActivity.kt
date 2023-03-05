7e92c9c541d2c205ccfcd385284778ac5feb3c98
crystal <crystal@test.com>
2023-03-05 20:19:38 +0900

    initial

--- OnDevice/app/src/main/java/com/example/facedetection/MainActivity.kt
+++ OnDevice/app/src/main/java/com/example/facedetection/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.facedetection
 
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.FileProvider
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.io.File
 import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +56,36 @@ class MainActivity : AppCompatActivity() {
                     // ...
                 }
         }
+
+
+        private var photoName: String? = null
+        private val takePhoto = registerForActivityResult(
+            ActivityResultContracts.TakePicture()
+        ) { didTakePhoto: Boolean ->
+            // Handle the result
+            if (didTakePhoto && photoName != null) {
+                // save filename into DB
+            }
+        }
+
+        val photoCamera: ImageButton = findViewById(R.id.btnTakePhoto)
+        photoCamera.setOnClickListener {
+            photoName = "IMG_${Date()}.JPG"
+            val photoFile = File(requireContext().applicationContext.filesDir, photoName)
+            val photoUri = FileProvider.getUriForFile(
+                requireContext(),
+                "com.bignerdranch.android.criminalintent.fileprovider",
+                photoFile
+            )
+            takePhoto.launch(photoUri)
+        }
+
+        val captureImageIntent = takePhoto.contract.createIntent(
+            requireContext(),
+            null
+        )
+        photoCamera.isEnabled = canResolveIntent(captureImageIntent)
+
     }
 
 
@@ -77,4 +116,15 @@ class MainActivity : AppCompatActivity() {
             }
         } catch (e: IOException) { null }
     }
+
+    private fun canResolveIntent(intent: Intent): Boolean {
+        val packageManager: PackageManager = requireActivity().packageManager
+        val resolvedActivity: ResolveInfo? =
+            packageManager.resolveActivity(
+                intent,
+                PackageManager.MATCH_DEFAULT_ONLY
+            )
+        return resolvedActivity != null
+    }
+
 }
